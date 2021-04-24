2c3276857efbe1f2c29ed88f6bcedd00e3e4f251
Arturo Estrada <arturoestradanav@gmail.com>
2021-04-23 23:59:09 -0500

    Create model test files

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ApplicationRecord
+  validates :title, :description, :url, presence: true
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,1 +1,29 @@
-project_spec.rb
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+  subject do
+    described_class.new(
+      title: 'Kataplum',
+      description: 'text',
+      url: 'http address'
+    )
+  end
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without an url' do
+    subject.url = nil
+    expect(subject).to_not be_valid
+  end
+end
